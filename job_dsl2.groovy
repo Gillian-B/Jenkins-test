5c0512b3766a7aa9a91ccfd0c245bb30d2398383
Gillian-B <bruyas.gillian@gmail.com>
2021-04-08 16:42:06 +0200

    update

--- job_dsl2.groovy
+++ job_dsl2.groovy
@@ -7,3 +7,15 @@ job('Tools/Job_DSL_TEST') {
     	shell('echo Hello World!')
     }
 }
+
+job('Tools/clone-repository') {
+    parameters {
+    	stringParam('GIT_REPOSITORY_URL', null, 'Git URL of the repository to clone')
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        shell 'git clone ${GIT_REPOSITORY_URL}'
+    }
+}
